b374cb218019982ae45b709b5aa19ec9a8a0957f
Juan <juangarpe94@gmail.com>
2015-03-18 14:53:09 +0100

    pokemon

---
+++ pokemon.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+#Seminario 
+#Juan Garcia y Braulio Pareja
+# Vamos a desarrollar el programa de Python que nos permita, a partir de una lista de 
+#palabras, encontrar la lista más larga posible de palabras de manera que la última letra de 
+#una palabra coincida con la primera de la siguiente palabra. Para probar el programa la lista 
+#de palabras con las que vamos a trabajar son la que encontramos en el fichero pokemon.txt 
+
+fichero=open('pokemon.txt','r')
+
+contenido = fichero.read()
+fichero.close()
+lista=[]
+
+for palabra in contenido.split(' '):
+    lista.append(palabra)
+
+lista.remove('\r\n')
+lista3=[]
+
+for elemento in lista:
+    lista1=lista[:]
+    lista2=[]
+    lista2.append(elemento)
+    lista1.remove(elemento)
+    
+    for elemento2 in lista:
+        for elemento2 in lista1:
+            if elemento[-1]==elemento2[0]:
+                lista2.append(elemento2)
+                elemento=elemento2
+                lista1.remove(elemento2)
+                
+    if len(lista2)>len(lista3):
+        
+        lista3=lista2[:]
+    
+print lista3
+print "Palabras: ",len(lista3)
+
+# <codecell>
+
+
+# <codecell>
+
+
